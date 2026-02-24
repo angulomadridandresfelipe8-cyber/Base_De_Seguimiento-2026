97836cae78a8b1827b43b9335436e20921f6bce7
AndresMadrid <angulomadridandresfelipe8@gmail.com>
2026-02-24 10:53:30 -0500

    base de datos 2

---
+++ conexion.php
@@ -0,0 +1,9 @@
+<?php
+$conexion = new PDO(
+    "mysql:dashboard2026.infinityfreeapp.com;dbname=epiz_12345678_dashboard2026",
+    "if0_41237493",
+    "Andresxd46"
+);
+
+$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
